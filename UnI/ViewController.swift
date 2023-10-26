61403e2ae1724c3ff8fa0863c50dfcb5d92c76d4
JaeWoong Jeong <jjw8959@naver.com>
2023-10-26 16:56:09 +0900

    Initial Commit

---
+++ UnI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnI
+//
+//  Created by woong on 10/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
